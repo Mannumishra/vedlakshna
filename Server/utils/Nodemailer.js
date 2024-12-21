540042960bb5dc72f0c4869e4f84a4c6f9285f64
mannumishra <mannumishra2207@outlook.com>
2024-12-21 16:02:50 +0530

    add chnages

--- Server/utils/Nodemailer.js
+++ Server/utils/Nodemailer.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.USER || 'bbirthday314@gmail.com', // Replace with your email
-        pass: process.env.PASS || 'mofg cwgh xtdw hbjp',    // Replace with App Password
+        user: 'mannu22072000@gmail.com', // Replace with your email
+        pass: 'zjbl jnup ksaz wrnr',    // Replace with App Password
     }, // Output detailed logs
 });
 
