efe65ae0a6bbd69a84df25509a94340da44d8310
mannumishra <mannumishra2207@outlook.com>
2024-12-28 16:40:52 +0530

    add chnages

--- Admin/src/Components/Home/Home.jsx
+++ Admin/src/Components/Home/Home.jsx
@@ -28,8 +28,8 @@ import EditPincode from '../../Pages/PinCode/EditPincode'
 import AllContactInquery from '../../Pages/ContactInquery/AllContactInquery'
 
 const Home = () => {
-  // const loginValue = localStorage.getItem("login")
-  const loginValue = true
+  const loginValue = localStorage.getItem("login")
+  // const loginValue = true
   return (
     <>
       {
